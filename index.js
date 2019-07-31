598f34096dc2bf65e974bcfde4072e3aae0b3bac
Diana Badas <diana.badas@gmail.com>
2019-07-31 09:10:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ function justInvoke(fn) {
   return fn();
 }
 
-function setThisWithCall(fn, thisValue, arg) {
-  return fn.call(thisValue, arg);
-};
+function setThisWithCall(fn, thisValue, argument) {
+  return fn.call(thisValue, argument);
+}
 
-function setThisWithApply(fn, thisValue, args) {
-  return fn.apply(thisValue, args);
-};
+function setThisWithApply(fn, thisValue, ourArguments) {
+  return fn.apply(thisValue, ourArguments);
+}
 
-funtion returnNewFunctionOf(functionToBeCopied, thisValue) {
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
   const copy = functionToBeCopied.bind(thisValue);
   return copy;
-};
+}
